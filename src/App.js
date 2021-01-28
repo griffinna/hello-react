cedede7a64e96e16565905fa4e86db6e3cc67ca8
Griffin <garam.dev9@gmail.com>
2021-01-28 23:26:44 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, Fragment } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    const text = 'hellooooooooooo';
+    return (
+      <Fragment>
+        <h1>HELLO REACT!</h1>
+        <h2>{text}</h2>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
